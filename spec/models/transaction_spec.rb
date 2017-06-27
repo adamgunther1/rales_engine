6ccb3fdba72f37346e66d5944846017b987d79e9
adamgunther1 <adamgunther1@gmail.com>
2017-06-26 19:00:35 -0600

    adds relationship testing to all models

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Transaction, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "relationships" do
+    it { should belongs_to(:invoice) }
+  end
 end
