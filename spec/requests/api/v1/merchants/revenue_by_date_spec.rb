482f1278edc16301e6c1f01e9460c37260a64f5d
NataliaColome <nmcolome@gmail.com>
2017-06-27 16:32:35 -0600

    add merchant/revenue?date=x  spec and controller

---
+++ spec/requests/api/v1/merchants/revenue_by_date_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe "Merchants/Revenue API" do
+  context "GET /api/v1/merchants/revenue?date=x" do
+    it "returns the total revenue for date 'x' across all merchants" do
+      merchant = create(:merchant_with_items)
+      invoice = create(:invoice, merchant_id: merchant.id)
+      InvoiceItem.create(invoice_id: invoice.id, item_id: merchant.items.first.id, quantity: 2, unit_price: 128934)
+      InvoiceItem.create(invoice_id: invoice.id, item_id: merchant.items.last.id, quantity: 3, unit_price: 823765)
+      get "/api/v1/merchants/revenue?date=#{Date.today}"
+
+      expect(response).to be_success
+      
+      raw_merchant = JSON.parse(response.body)
+# binding.pry
+      expect(raw_merchant).to have_key("revenue")
+      expect(raw_merchant).to have_key("date")
+      expect(raw_merchant["revenue"]).to be_a Float
+      expect(raw_merchant["date"]).to be_a String
+      expect(raw_merchant["revenue"]).to eq(27291.63)
+      expect(raw_merchant["date"]).to eq(Date.today.to_s)
+    end
+  end
+end
