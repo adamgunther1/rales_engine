8276a57eef9319384a22b0602d7141db599de6d4
NataliaColome <nmcolome@gmail.com>
2017-06-26 18:24:40 -0600

    add invoice_record_endpoints request specs

---
+++ spec/requests/api/v1/invoices_request_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+RSpec.describe "Invoices API" do
+  context "GET /api/v1/invoices" do
+    it "sends all invoices" do
+      create_list(:invoice, 3)
+
+      get "/api/v1/invoices"
+
+      expect(response).to be_success
+
+      raw_invoices = JSON.parse(response.body)
+      raw_invoice = raw_invoices.first
+
+      expect(raw_invoices.count).to eq(3)
+      expect(raw_invoice).to have_key("customer_id")
+      expect(raw_invoice).to have_key("merchant_id")
+      expect(raw_invoice).to have_key("status")
+      expect(raw_invoice["customer_id"]).to be_a Integer
+      expect(raw_invoice["merchant_id"]).to be_a Integer
+      expect(raw_invoice["status"]).to be_a Integer
+    end
+  end
+
+  context "GET /api/v1/invoices/:id" do
+    it "sends an invoice" do
+      invoice = create(:invoice)
+
+      get "/api/v1/invoice/#{invoice.id}"
+
+      raw_invoice = JSON.parse(response.body)
+
+      expect(response).to be_success
+      expect(raw_invoice["id"]).to eq(invoice.id)
+      expect(raw_invoice).to have_key("customer_id")
+      expect(raw_invoice).to have_key("merchant_id")
+      expect(raw_invoice).to have_key("status")
+      expect(raw_invoice["customer_id"]).to be_a Integer
+      expect(raw_invoice["merchant_id"]).to be_a Integer
+      expect(raw_invoice["status"]).to be_a Integer
+    end
+  end
+end
