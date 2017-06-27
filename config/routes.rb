2edb3dd4aab79556844d6eaca1193a6099cc5a4d
NataliaColome <nmcolome@gmail.com>
2017-06-26 19:21:15 -0600

    adds routes and controller#actions to items

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :merchants, only: [:index, :show]
-    end
-  end
-
-  namespace :api do
-    namespace :v1 do
       resources :invoice_items, only: [:index, :show]
+      resources :items, only: [:index, :show]
     end
   end
 end
