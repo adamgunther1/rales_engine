ff2532d6256daa8dc14c06539532a2c4d8a7bf14
NataliaColome <nmcolome@gmail.com>
2017-06-29 09:13:09 -0600

    Move logic into models, refactored merchant controller and merchant model

--- app/controllers/api/v1/merchants/revenue_controller.rb
+++ app/controllers/api/v1/merchants/revenue_controller.rb
@@ -1,22 +1,15 @@
 class Api::V1::Merchants::RevenueController < ApplicationController
 
   def index
-        binding.pry
-        @something = Merchant.revenue_for_all_by_date(params[:date])
-        binding.pry
-    render json: @something
+    render json: Merchant.revenue_for_all_by_date(params[:date])
   end
 
   def show
-    sql_wo_date = "SELECT merchants.name merchant_name, SUM(invoice_items.quantity * invoice_items.unit_price) AS revenue FROM merchants INNER JOIN items ON merchants.id = items.merchant_id INNER JOIN invoice_items ON items.id = invoice_items.item_id INNER JOIN invoices ON invoices.id = invoice_items.invoice_id INNER JOIN transactions ON transactions.invoice_id = invoices.id WHERE merchants.id =  #{params[:id]} AND transactions.result = 0 GROUP BY merchant_name;"
-    sql_w_date = "SELECT merchants.name merchant_name, invoices.updated_at AS date, SUM(invoice_items.quantity * invoice_items.unit_price) AS revenue FROM merchants INNER JOIN items ON merchants.id = items.merchant_id INNER JOIN invoice_items ON items.id = invoice_items.item_id INNER JOIN invoices ON invoices.id = invoice_items.invoice_id WHERE invoices.updated_at = #{params[:date]} AND merchants.id = #{params[:id]} GROUP BY merchant_name, date;"
     if params[:date]
       result = ActiveRecord::Base.connection.execute(sql_w_date).values
     else
       result = ActiveRecord::Base.connection.execute(sql_wo_date).values
     end
-
-    output = {revenue: result.first.last/100.to_f}
 
     render json: output
   end
--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -3,16 +3,36 @@ class Merchant < ApplicationRecord
   has_many :items
 
   def self.revenue_for_all_by_date(date)
-    sql = "SELECT SUM(invoice_items.quantity * invoice_items.unit_price) AS total_revenue FROM merchants INNER JOIN items ON merchants.id = items.merchant_id INNER JOIN invoice_items ON items.id = invoice_items.item_id INNER JOIN invoices ON invoices.id = invoice_items.invoice_id INNER JOIN transactions ON transactions.invoice_id = invoices.id WHERE invoices.updated_at = '2012-03-16 11:55:05' AND transactions.result = 0;"
-    binding.pry
-    Merchant.find_by_sql(sql)
+    Merchant.find_by_sql [
+      "SELECT ROUND(SUM(invoice_items.quantity * invoice_items.unit_price / 100.00), 2) AS total_revenue 
+      FROM merchants 
+      INNER JOIN items ON merchants.id = items.merchant_id 
+      INNER JOIN invoice_items ON items.id = invoice_items.item_id 
+      INNER JOIN invoices ON invoices.id = invoice_items.invoice_id 
+      INNER JOIN transactions ON transactions.invoice_id = invoices.id 
+      WHERE invoices.updated_at = ? AND transactions.result = 0;", date ]
   end
 
-  def self.revenue_for_one
-    
+  def self.revenue_for_one(id)
+    Merchant.find_by_sql [
+      "SELECT merchants.name merchant_name, ROUND(SUM(invoice_items.quantity * invoice_items.unit_price / 100.00), 2) AS revenue 
+      FROM merchants 
+      INNER JOIN items ON merchants.id = items.merchant_id 
+      INNER JOIN invoice_items ON items.id = invoice_items.item_id 
+      INNER JOIN invoices ON invoices.id = invoice_items.invoice_id 
+      INNER JOIN transactions ON transactions.invoice_id = invoices.id 
+      WHERE merchants.id = ? AND transactions.result = 0 
+      GROUP BY merchant_name;", id ]
   end
 
-  def self.revenue_for_one_by_date
-    
+  def self.revenue_for_one_by_date(id, date)
+    Merchant.find_by_sql [
+      "SELECT merchants.name merchant_name, invoices.updated_at AS date, ROUND(SUM(invoice_items.quantity * invoice_items.unit_price / 100.00), 2) AS revenue 
+      FROM merchants 
+      INNER JOIN items ON merchants.id = items.merchant_id 
+      INNER JOIN invoice_items ON items.id = invoice_items.item_id 
+      INNER JOIN invoices ON invoices.id = invoice_items.invoice_id 
+      WHERE invoices.updated_at = ? AND merchants.id = ? 
+      GROUP BY merchant_name, date;", date, id ]
   end
 end
