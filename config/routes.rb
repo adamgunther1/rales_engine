81945735085b15679b75ad32b12cc7989058b918
NataliaColome <nmcolome@gmail.com>
2017-06-29 19:04:18 -0600

    add end

--- config/routes.rb
+++ config/routes.rb
@@ -35,6 +35,7 @@ Rails.application.routes.draw do
       namespace :invoice_items do
         get '/find', to: 'find#show'
         get '/find_all', to: 'find_all#show'
+      end
 
       namespace :transactions do
         get '/find', to: 'find#show'
